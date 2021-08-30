978e96c7ec335c64ce308ebdcda03b4f57d73672
CJ <ngassajason@yahoo.fr>
2021-08-30 12:29:16 +0100

    ✨ adds get all events method - ⚡️ improves performance

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,12 +1,14 @@
 <template>
-    <h1>Events For Good</h1>
-    <div class="events">
-        <Event v-for="event in events" :key="event.id" :event="event" />
-    </div>
+  <h1>Events For Good</h1>
+  <div class="events">
+      <Event v-for="event in events" :key="event.id" :event="event" />
+  </div>
 </template>
 
 <script>
 import Event from "@/components/Event";
+import EventService from "@/services/EventService.js";
+
 export default {
   name: 'EventCard',
   components: {
@@ -14,43 +16,18 @@ export default {
   },
   data() {
     return {
-      events: [
-        {
-          id: 5928101,
-          category: 'animal welfare',
-          title: 'Cat Adoption Day',
-          description: 'Find your new feline friend at this event.',
-          location: 'Meow Town',
-          date: 'January 28, 2022',
-          time: '12:00',
-          petsAllowed: true,
-          organizer: 'Kat Laydee'
-        },
-        {
-          id: 4582797,
-          category: 'food',
-          title: 'Community Gardening',
-          description: 'Join us as we tend to the community edible plants.',
-          location: 'Flora City',
-          date: 'March 14, 2022',
-          time: '10:00',
-          petsAllowed: true,
-          organizer: 'Fern Pollin'
-        },
-        {
-          id: 8419988,
-          category: 'sustainability',
-          title: 'Beach Cleanup',
-          description: 'Help pick up trash along the shore.',
-          location: 'Playa Del Carmen',
-          date: 'July 22, 2022',
-          time: '11:00',
-          petsAllowed: false,
-          organizer: 'Carey Wales'
-        }
-      ]
+      events: null
      }
    },
+   created() {
+     EventService.getEvents()
+     .then(response => {
+       console.log(response);
+       this.events = response.data;
+      })
+      .catch(error=>{ console.error(error);})
+   }
+
 }
 </script>
 
